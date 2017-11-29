e1596d27e45244bb6e6365e41cbdf65c91ee50ac
ElenaGoroshko <elena.goroshko@gmail.com>
2017-11-29 18:34:13 +0200

    layout constraint work

--- ProjectMultithreading/ViewController.swift
+++ ProjectMultithreading/ViewController.swift
@@ -10,14 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var ibStackView: UIStackView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if size.height < size.width {
+           ibStackView.axis = .horizontal
+        } else {
+            ibStackView.axis = .vertical
+        }
     }
 
 }
