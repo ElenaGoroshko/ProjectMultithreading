213e48f9a713eacaf22b5970084f93759b477727
ElenaGoroshko <elena.goroshko@gmail.com>
2017-11-29 15:53:19 +0200

    start

---
+++ ProjectMultithreading/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ProjectMultithreading
+//
+//  Created by Elena Goroshko on 11/29/17.
+//  Copyright © 2017 Elena Goroshko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
