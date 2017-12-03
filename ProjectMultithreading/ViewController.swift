c82c0e3a6504ff04c05bb8fda66ebe66eacdebc2
ElenaGoroshko <elena.goroshko@gmail.com>
2017-12-03 18:20:01 +0200

    work

--- ProjectMultithreading/ViewController.swift
+++ ProjectMultithreading/ViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet private weak var ibStackView: UIStackView!
+
     @IBOutlet private weak var imageView1: UIImageView!
     @IBOutlet private weak var imageView2: UIImageView!
     @IBOutlet private weak var imageView3: UIImageView!
+
+    @IBOutlet weak var activityIndicatorView1: UIActivityIndicatorView!
+    @IBOutlet weak var activityIndicatorView2: UIActivityIndicatorView!
+    @IBOutlet weak var activityIndicatorView3: UIActivityIndicatorView!
+
     
     private let imageArray1 = [
         "http://www.kartinki.me/mini/201508/21765.jpg",
@@ -34,8 +40,14 @@ class ViewController: UIViewController {
         "http://www.kartinki.me/mini/201312/16606.jpg"
     ]
 
+    private var imageIndex1 = 0
+    private var imageIndex2 = 0
+    private var imageIndex3 = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadAllImage()
+
     }
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         if size.height < size.width {
@@ -46,5 +58,52 @@ class ViewController: UIViewController {
     }
 
     @IBAction func pefreshPresed(_ sender: UIButton) {
+        imageIndex1 = (imageIndex1 + 1) % imageArray1.count
+        imageIndex2 = (imageIndex2 + 1) % imageArray2.count
+        imageIndex3 = (imageIndex3 + 1) % imageArray3.count
+        loadAllImage()
+    }
+
+    // MARK: private methods
+    private func loadAllImage() {
+        loadImage(imageView: imageView1, imageArray: imageArray1, indexImage: imageIndex1,
+                  indicator: activityIndicatorView1)
+
+        loadImage(imageView: imageView2, imageArray: imageArray2, indexImage: imageIndex2,
+                  indicator: activityIndicatorView2)
+
+        loadImage(imageView: imageView3, imageArray: imageArray3, indexImage: imageIndex3,
+                  indicator: activityIndicatorView3)
+    }
+
+    private func indicatorStart(indicator: UIActivityIndicatorView) {
+        indicator.startAnimating()
+        indicator.isHidden = false
+    }
+    private func indicatorStop(indicator: UIActivityIndicatorView) {
+        DispatchQueue.main.async {
+            indicator.stopAnimating()
+            indicator.isHidden = true
+        }
+    }
+    private func loadImage(imageView: UIImageView, imageArray: [String],
+                           indexImage: Int, indicator: UIActivityIndicatorView ) {
+        imageView.image = nil
+        guard indexImage >= 0, indexImage < imageArray.count else { return }
+        let imageStringUrl = imageArray[indexImage]
+        guard let url = URL(string: imageStringUrl) else { return }
+        indicatorStart(indicator: indicator)
+
+        DispatchQueue.global().async { [weak self] in
+            guard let imageData = try? Data(contentsOf: url) else {
+                self?.indicatorStop(indicator: indicator)
+                return
+            }
+            let loadedImage = UIImage(data: imageData)
+            DispatchQueue.main.async { [weak self] in
+               imageView.image = loadedImage
+               self?.indicatorStop(indicator: indicator)
+            }
+        }
     }
 }
