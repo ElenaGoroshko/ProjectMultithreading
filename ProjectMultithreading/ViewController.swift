28e3fcfd80fb52b8704b0af5b262e350c2106575
ElenaGoroshko <elena.goroshko@gmail.com>
2017-12-03 13:09:08 +0200

    no message

--- ProjectMultithreading/ViewController.swift
+++ ProjectMultithreading/ViewController.swift
@@ -11,6 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet private weak var ibStackView: UIStackView!
+    @IBOutlet private weak var imageView1: UIImageView!
+    @IBOutlet private weak var imageView2: UIImageView!
+    @IBOutlet private weak var imageView3: UIImageView!
+    
+    private let imageArray1 = [
+        "http://www.kartinki.me/mini/201508/21765.jpg",
+        "http://www.kartinki.me/mini/201409/20844.jpg",
+        "http://www.kartinki.me/mini/201501/21333.jpg",
+        "http://www.kartinki.me/mini/201309/15787.jpg"
+    ]
+    private let imageArray2 = [
+        "http://www.kartinki.me/mini/201307/14415.jpg",
+        "http://www.kartinki.me/mini/201308/14991.jpg",
+        "http://www.kartinki.me/mini/201306/12019.jpg",
+        "http://www.kartinki.me/mini/201209/8133.jpg"
+    ]
+    private let imageArray3 = [
+        "http://www.kartinki.me/mini/201405/20025.jpg",
+        "http://www.kartinki.me/mini/201509/21808.jpg",
+        "http://www.kartinki.me/mini/201508/21766.jpg",
+        "http://www.kartinki.me/mini/201312/16606.jpg"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
